b829fe7d6cc169e0806fb9c33ccf9a1530b800a2
Charli Sung <charlisung@gmail.com>
2023-05-28 13:04:32 -0700

    add test page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/index";
 import About from "./pages/About";
 import "./App.css";
+import TestPage from "./pages/Test";
+import Test from "./pages/Test/[id]";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/test/:id" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
